dd0c0b56ecefea73f34b74f53a0a387443b01f4e
Rami <138556019+rami242424@users.noreply.github.com>
2024-11-13 12:03:48 +0900

    #2.5

--- components/navigation.tsx
+++ components/navigation.tsx
@@ -1,12 +1,11 @@
-"use client"
+// "use client"
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import { useState } from "react";
 
 export default function Navigation(){
     const path = usePathname();
-    console.log(path);
-    const [count, setCount] = useState(0);
+    // console.log(path);
     return(
         <nav>
             <ul>
@@ -16,8 +15,7 @@ export default function Navigation(){
                 <li>
                     <Link href="/about-us">About Us</Link>{path === "/about-us" ? "💙💙" : ""}
                 </li>
-                <li><button onClick={() => setCount(c=> c+1)}>{count}</button></li>
             </ul>
-        </nav>
+        </nav> 
     )
 }
