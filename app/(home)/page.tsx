9579a4029a1eecdcbf5d32774692b2d7333e2abc
Rami <138556019+rami242424@users.noreply.github.com>
2024-11-13 20:58:54 +0900

    3.2 Server Side

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -5,6 +5,9 @@ export const metadata = {
 const URL = "https://nomad-movies.nomadcoders.workers.dev/movies";
 
 async function getMovies(){
+    // 프로그램을 멈춰서 느리게 만드는 간단한 트릭
+    await new Promise((resolve) => setTimeout(resolve, 5000));
+
     // return fetch(URL).then(response => response.json());
     // 같은 코드
     const response = await fetch(URL);
