0146a38ce9776a9fbb44392d9150fc5880922ecf
Rami <138556019+rami242424@users.noreply.github.com>
2024-11-14 12:01:34 +0900

    #3.7 Error Handling: 3초 뒤 에러

--- app/(movies)/movies/[id]/page.tsx
+++ app/(movies)/movies/[id]/page.tsx
@@ -1,24 +1,23 @@
-import { API_URL } from "../../../(home)/page";
-async function getMovie(id: string) {
-  console.log(`Fetching movies: ${Date.now()}`);
-  await new Promise((resolve) => setTimeout(resolve, 5000));
-  const response = await fetch(`${API_URL}/${id}`);
-  return response.json();
-}
-async function getVideos(id: string) {
-  console.log(`Fetching videos: ${Date.now()}`);
-  await new Promise((resolve) => setTimeout(resolve, 5000));
-  const response = await fetch(`${API_URL}/${id}/videos`);
-  return response.json();
-}
+import { Suspense } from "react";
+import MovieInfo from "../../../../components/movie-info";
+import MovieVideo from "../../../../components/movie-video";
+
 export default async function MovieDetail({
-  params: { id },
-}: {
-  params: { id: string };
-}) {
-  console.log("===========");
-  console.log("start fetching");
-  const [movie, videos] = await Promise.all([getMovie(id), getVideos(id)]);
-  console.log("end fetching");
-  return <h1>{movie.title}</h1>;
+    params: {id}, 
+    } : {
+    params: {id: string};
+    }) {
+    
+
+    return (
+    <div>
+        <h3>Movie Detail Page</h3>
+        <Suspense fallback={<h1>Loading movie info</h1>}>
+            <MovieInfo id={id}/>
+        </Suspense>
+        <Suspense fallback={<h1>Loading movie videos</h1>}>
+            <MovieVideo id={id}/>    
+        </Suspense>
+    </div>
+    );
 }
